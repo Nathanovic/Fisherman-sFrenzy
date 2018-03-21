e7cae9754fae928800bca9f26b009a5cd776fc56
Nathanovich <n.flier@live.nl>
2018-03-21 14:43:49 +0100

    watersprayprefab update

---
+++ Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//this script is used to activate, walk through and finish the tutorial
+public class Tutorial : MonoBehaviour {
+
+	public CanvasGroup panel;
+
+	public Image[] tutorialImages;
+	private int tutorialCurrent = 0;
+	public int tutorialViewCount = 3;
+
+	void Awake(){
+		panel = GetComponent<CanvasGroup> ();
+	}
+
+	void Start(){
+		Debug.Log ("build version: " + Application.version);
+		tutorialImages = GetComponentsInChildren<Image> ();
+	}
+
+	public void Continue(){
+		tutorialCurrent++;
+		if (tutorialCurrent == tutorialViewCount) {
+			panel.Deactivate ();
+		}
+	}
+}
